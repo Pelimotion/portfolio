1a299f10a3f405d47ee2d2f616c908edfd07e399
Felipe Conceição <felipeconceicao@MacBook-Air-de-Felipe.local>
2026-05-07 21:29:34 -0300

    Site deploy (no media): 2026-05-07 21:29:34

--- download_ffmpeg_assets.py
+++ download_ffmpeg_assets.py
@@ -2,14 +2,17 @@ import os
 import urllib.request
 
 urls = [
-    "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js",
-    "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm"
+    "https://unpkg.com/@ffmpeg/ffmpeg@0.12.10/dist/umd/ffmpeg.js",
+    "https://unpkg.com/@ffmpeg/ffmpeg@0.12.10/dist/umd/814.ffmpeg.js",
+    "https://unpkg.com/@ffmpeg/util@0.12.1/dist/umd/index.js"
 ]
 
 os.makedirs("ffmpeg_assets", exist_ok=True)
 
 for url in urls:
     filename = url.split("/")[-1]
+    if filename == 'index.js':
+        filename = 'ffmpeg-util.js' # Rename index.js to ffmpeg-util.js for clarity in Bunny.net
     filepath = os.path.join("ffmpeg_assets", filename)
     print(f"Downloading {filename}...")
     urllib.request.urlretrieve(url, filepath)
